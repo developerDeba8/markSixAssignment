90c869975ff630d53940697b650f3596f35fb080
Debashis Saha <developerdeba8@gmail.com>
2021-08-29 17:17:50 +0530

    feat - added script file

---
+++ app.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#txt-input");
+var outputDivText = document.querySelector("#txt-output")
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function constructTranslationTextURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured: ", error);
+}
+
+function translateButtonClickHandler(){
+    fetch(constructTranslationTextURL(inputText.value))
+    .then(response => response.json())
+    .then(json => {
+        outputDivText.innerText = json.contents.translated;})
+    .catch(errorHandler)
+}
+
+
+btnTranslate.addEventListener("click", translateButtonClickHandler);
